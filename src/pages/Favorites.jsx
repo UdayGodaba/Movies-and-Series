5157af0d916b087ac318626ec7ac42ee19e27e31
UdayGodaba <godabauday@gmail.com>
2024-06-04 03:55:16 +0530

    FEAT: filter in Features added

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,14 +1,83 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useSelector } from "react-redux";
 import DisplayCards from "../components/DisplayCards";
+import { MenuItem, Select } from "@mui/material";
+import { genres, ratings } from "../utils/constants";
+import { useEffect, useState } from "react";
 
 const Favorites = () => {
   const { data } = useSelector((state) => state.favorite);
-  console.log(data);
+  const [genre, setGenre] = useState("Any");
+  const [rating, setRating] = useState(0);
+  const [title, setTitle] = useState("");
+  const [favData, setFavData] = useState(data);
+
+  useEffect(() => {
+    let updatedData;
+    updatedData = data
+      .filter((elem) => elem?.imdbRating >= rating)
+      .filter((elem) => {
+        if (genre === "Any") {
+          return elem;
+        } else {
+          return elem?.Genre.toLowerCase().includes(genre.toLowerCase());
+        }
+      })
+      .filter((elem) => {
+        if (title === "") {
+          return elem;
+        } else {
+          return elem?.Title.toLowerCase().includes(title.toLowerCase());
+        }
+      });
+    setFavData(updatedData);
+  }, [title, rating, genre]);
+
   return (
     <div className="fav">
-      <div className="fav-sc1"></div>
+      <div className="fav-sc1">
+        <input
+          placeholder="Enter title..."
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <Select
+          value={genre}
+          sx={{
+            backgroundColor: "orange",
+            borderRadius: "15px",
+            margin: "5px",
+            height: "45px",
+            width: "130px",
+          }}
+          onChange={(e) => setGenre(e.target.value)}
+        >
+          {genres.map((elem, idx) => (
+            <MenuItem value={elem} key={idx}>
+              {elem}
+            </MenuItem>
+          ))}
+        </Select>
+        <Select
+          value={rating}
+          sx={{
+            backgroundColor: "orange",
+            borderRadius: "15px",
+            margin: "5px",
+            height: "45px",
+            width: "70px",
+          }}
+          onChange={(e) => setRating(e.target.value)}
+        >
+          {ratings.map((elem, idx) => (
+            <MenuItem value={elem} key={idx}>
+              {elem}
+            </MenuItem>
+          ))}
+        </Select>
+      </div>
       <div className="fav-sc2">
-        <DisplayCards cards={data} />
+        <DisplayCards cards={favData} />
       </div>
     </div>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,15 @@
-export const API_URL = "https://www.omdbapi.com/?apikey=" + import.meta.env.VITE_APP_API_KEY;
+export const API_URL =
+  "https://www.omdbapi.com/?apikey=" + import.meta.env.VITE_APP_API_KEY;
+
+export const ratings = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export const genres = [
+  "Any",
+  "Action",
+  "Adventure",
+  "Crime",
+  "Drama",
+  "Fantasy",
+  "Thriller",
+  "Romance",
+];
